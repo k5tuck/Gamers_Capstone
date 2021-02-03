42185e7a2146e542c81be3040bdacd619f70a508
Ian Storms <IanStorms110@yahoo.com>
2021-02-03 15:20:14 -0500

    updated signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import axios from 'axios';
+import SignUpPage from './components/SignUpPage';
 
 function App() {
   return (
     <div className="App">
+      <SignUpPage/>
      Gamers Capstone
     </div>
   );
