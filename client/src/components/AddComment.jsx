175371c428ad9ceced00026c3d686d213d7bfb71
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2021-02-03 13:00:08 -0500

    about, contact, add-comment

--- client/src/components/AddComment.jsx
+++ client/src/components/AddComment.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function AddComment() {
+function AddComment(props) {
+    const [content, setContent] = useState('')
+    
+
     return (
-        
+        <div className="">
+            <h1>Comment on: ${post.title}</h1>
+            <div className="">
+            ${post.media.includes("/uploads/media/") ? `<img
+                src="${post.media}"
+                alt="${post.displayname}'s image"
+            />`:'' }
+            </div>
+            <p>${post.content}</p>
+
+            <form onSubmit={props.processComment}>
+            <label>
+                Comment:
+                <textarea name="content" value={content} onChange={e => {
+                    setContent(e.target.value)
+                }}></textarea>
+            </label>
+            <br />
+            <input type="submit" value="submit" />
+            </form>
+        </div>
     )
 }
 
