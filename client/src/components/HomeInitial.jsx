19a6137c2c4bc205b8156fa80313357a6a00af90
Joshua Lopez <jrenelopez92@gmail.com>
2021-02-03 12:02:03 -0600

    home page still working on stuff

--- client/src/components/HomeInitial.jsx
+++ client/src/components/HomeInitial.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function HomeInitial() {
     return (
-        
+        <>
+        {/* <nav> This is my nav bar</nav> */}
+            <h1>Welcome to GamersParadise</h1>
+            <body>
+                <article>
+                    Welcome to gamers GamersParadise, This is a true Video game social media platform to share 
+                    video game content connect with average and professional players alike sharing video game any video game content (news, stories, release dates leaks)
+                </article>
+                <p>
+                    This is where the top 4 will show ~~~~
+                </p>
+                
+            </body>
+            <footer>
+                <Link to="/about"> About Us</Link>
+                <Link to="/Contact"> Contact Us </Link>
+            </footer>
+
+        </>
     )
 }
 
