67e1a091d45acb17692ed35c5a46b5c4a473775c
Ian Storms <IanStorms110@yahoo.com>
2021-02-04 12:31:59 -0500

    added redirect to login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,33 +1,54 @@
 import React from "react";
+import { Link, useHistory } from 'react-router-dom';
+import { useState } from 'react'
+import axios from "axios";
 
-function Login() {
+function Login(props) {
+  
+  
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const processLogin = async (e) => {
+    let user = {
+      username,
+      password
+    }
+    const resp = await axios.post('/api/login', user)
+
+  }
+
+
+  
   return (
     <div>
       <h1 align="center">Welcome Please Sign In To Your Gamer's Account</h1>
       <br />
 
-      <form align="center" method="POST">
+      <form onSubmit={processLogin}>
         <input
           type="text"
           name="username"
           autofocus
           placeholder="username/email"
+          onChange={(e)=>{
+            setUsername(e.target.value)
+          }}
         />
         <br />
-        <input type="password" name="password" />
+        <input type="password" name="password" onChange={(e)=>{
+          setPassword(e.target.value)
+        }}/>
         <br />
         <input type="submit" value="Login" />
       </form>
       <br />
       <div align="center">
-        <a href="/">
-          <button>BACK</button>
-        </a>
-      </div>
-
-      <div align="center">
         <p>
-          If you need to Sign Up Please Click <a href="/signup">HERE</a>
+          If you need to Sign Up Please Click :
+          
+            <Link to="/signup">HERE</Link>
+          
         </p>
       </div>
     </div>
