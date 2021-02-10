57956d27128fab1b4e9a5d9aba326692b9d3be67
Shoel Uddin <shoeluddin@Shoels-MacBook-Pro.local>
2021-02-09 22:19:12 -0500

    edit profile page

--- client/src/components/subcomponents/ProfilePagePic.jsx
+++ client/src/components/subcomponents/ProfilePagePic.jsx
@@ -1,20 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
+import Modal from "react-modal";
+import EditProfile from './EditProfile'
 
 function ProfilePagePic() {
     const { id } = useParams();
+    const [modalIsOpen, setModalIsOpen] = useState(false);
 
     const [displayName, setDisplayName] = useState('')
     const [Photo, setPhoto] = useState('')
-
+    
     const getData = async (e) =>{
         const resp = await axios.get(`/api/photo/${id}`)
         console.log(resp)
         const { photo, displayname } = resp.data;
         setDisplayName(displayname);
-        setPhoto(photo)
+        let profilePhoto = photo ? photo : " "
+        setPhoto(profilePhoto)
+    }
+    const editProfileDetails = async (details) => {
+        const resp = await axios.put(`/api/editprofile/${id}`, details, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        })
+        console.log(resp, "====== this is the editprofile +++++");
+        setDisplayName (resp.data.displayname)
+        setPhoto (resp.data.photo)
+
     }
 
     useEffect(()=>{
@@ -28,7 +42,27 @@ function ProfilePagePic() {
             <div className="profilepiccontainer">
                 <img className="profilepicimage" src={Photo} alt="prof pic"/>
             </div>
+        <button onClick={(e) => {
+            e.preventDefault()
+            // Function
+            //setModalIsOpen(true);
+            }
+        }> Edit Profile</button>
+        <Modal
+            isOpen={modalIsOpen}
+            // shouldCloseOnOverlayClick={false} // Click on Overlay will not Close the Modal
+            onRequestClose={() => {
+            setModalIsOpen(false);
+            }}
+        ><EditProfile
+            setPhoto = {setPhoto}
+            setDisplayName = {setDisplayName}
+            displayName={displayName}
+            photo={Photo}
+            editProfileDetails={editProfileDetails}
+        /></Modal>
         </div>
+        
 
     )
 };
