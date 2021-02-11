ae220ccb443be9cbf45344ec82c3fa55c35343c6
Kevin Tucker <kevin.tucker19@gmail.com>
2021-02-11 17:54:35 -0500

    added a little css to followers and following pages

--- client/src/components/subcomponents/WidgetChat.js
+++ client/src/components/subcomponents/WidgetChat.js
@@ -1,41 +1,54 @@
-import React, { useEffect, useCallback, useRef, useState } from 'react';
-import { Widget, addUserMessage } from 'react-chat-widget';
-import { StreamChat } from 'stream-chat';
+import React, { useEffect, useCallback, useRef, useState } from "react";
+import { Widget, addUserMessage } from "react-chat-widget";
+import { StreamChat } from "stream-chat";
 
-import 'react-chat-widget/lib/styles.css';
+import "react-chat-widget/lib/styles.css";
+
+import logo from "../../images/giphy.gif";
 
 const user = {
-    id: 'istorms',
-    name: 'Ian Storms'
-  };
+  id: "istorms",
+  name: "Ian Storms",
+};
 
 // const STREAM_API = process.env.REACT_APP_STREAM_API_SECRET
 // client = stream.connect('deknak4gnvw2', 'q6s865rwff2cz6t5382pfgza6365phtyv24ar9jjrr6msr3hmaa7qad3yg5up6ku', '111031');
 function WidgetChat() {
-
   const [messages, setMessages] = useState(null);
 
   // Create client variable from StreamChat using the api
-  const client = new StreamChat('deknak4gnvw2');
-//   'q6s865rwff2cz6t5382pfgza6365phtyv24ar9jjrr6msr3hmaa7qad3yg5up6ku', '111031'
+  const client = new StreamChat("deknak4gnvw2");
+  //   'q6s865rwff2cz6t5382pfgza6365phtyv24ar9jjrr6msr3hmaa7qad3yg5up6ku', '111031'
   const { id, name } = user;
-//   const token2 = client.createToken('istorms', Math.floor(Date.now() / 1000) + (60 * 60));
+  //   const token2 = client.createToken('istorms', Math.floor(Date.now() / 1000) + (60 * 60));
   const channel = useRef(null);
+  // const channel = useRef(channelWatch);
 
-  // Method - Set the user with the strean chat client variable 
+  // Method - Set the user with the strean chat client variable
   const setUser = useCallback(async () => {
-    await client.setUser(
-      { id, name },
-       client.devToken('istorms') 
-    );
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    await client.setUser({ id, name }, client.devToken("istorms"));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id, name]);
+
+  // Method - We make use of chat-widget "handleNewUserMessage" prop to update
+  // the chat and send the message to the channel
+  const handleNewUserMessage = useCallback(
+    async (message) =>
+      await channel.current.sendMessage({
+        text: message,
+      }),
+    []
+  );
 
   // Method - Set the channel, in this case we are setting a messaging
   // default chat provided by StreamChat
   const setChannel = useCallback(async () => {
-    channel.current = client.channel('messaging', 'wolox-support', {
-      name: 'Wolox customer support',
+    // channel.current = client.channel("messaging", "gamers-members", {
+    //   name: "Gamers Paradise",
+    // });
+
+    channel.current = client.channel("messaging", "wolox-support", {
+      name: "Wolox customer support",
     });
 
     const channelWatch = await channel.current.watch();
@@ -47,33 +60,29 @@ function WidgetChat() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // Method - We make use of chat-widget "handleNewUserMessage" prop to update 
-  // the chat and send the message to the channel
-  const handleNewUserMessage = useCallback(async message =>
-    await channel.current.sendMessage({
-      text: message
-    }), []);
-
   // Effect - Set the user and channel on first render
   useEffect(() => {
     setUser();
     setChannel();
-  }, [setUser, setChannel]);
+  }, [setUser, setChannel, setMessages]);
 
   // Effect - Map through the messages and add them to the widget using 'addUserMessage'
-  useEffect(
-    () => messages?.map(message => addUserMessage(message.text)), 
-    [messages]
-  );
+  useEffect(() => messages?.map((message) => addUserMessage(message.text)), [
+    messages,
+  ]);
 
-    return (
-        <div>
-            <Widget 
-            handleNewUserMessage={handleNewUserMessage}
-            title="Gamer's Paradise"
-            subtitle="Chat Center"/>
-        </div>
-    )
+  return (
+    <div>
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        profileAvatar={logo}
+        title="Gamer's Paradise"
+        // titleAvatar={}
+        subtitle="The Oasis"
+        senderPlaceHolder="Chat It Up!"
+      />
+    </div>
+  );
 }
 
 export default WidgetChat;
